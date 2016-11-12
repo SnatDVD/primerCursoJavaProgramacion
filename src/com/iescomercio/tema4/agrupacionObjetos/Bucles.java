7de59039d5f2394592d9fa3d8b72f402c3a87f3a
Miguel Vegas <chiqui@10.0.1.2>
2016-11-12 04:34:07 +0100

    Clase Notebook

---
+++ src/com/iescomercio/tema4/agrupacionObjetos/Bucles.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iescomercio.tema4.agrupacionObjetos;
+
+/**
+ *
+ * @author chiqui
+ */
+public class Bucles {
+    
+    public void ejercicio1(int num){
+        int aux = 0;
+        while(num > aux){
+            System.out.println(aux);
+            aux++;
+        }
+    }
+    
+    public void ejercio2(){
+        
+    }
+}
---
+++ src/com/iescomercio/tema4/agrupacionObjetos/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iescomercio.tema4.agrupacionObjetos;
+
+/**
+ *
+ * @author chiqui
+ */
+public class Main {
+    public static void main(String[] args) {
+        Bucles b1 = new Bucles();
+        
+        //b1.ejercicio1(50);
+    }
+}
